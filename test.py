d877e2597e2129231092ce5e1d5af7227596f363
ANIKET PANJWANI <aniketpanjwani20@gmail.com>
2025-05-06 11:11:48 +0530

    testing stash

--- test.py
+++ test.py
@@ -1,3 +1,11 @@
 #testing
 this file is for testing purposes only
 testing revert changes git revert command
+
+
+
+
+## testing git resert commmand 
+## testing git reset command
+testing git stash command
+#testing git stash pop command
